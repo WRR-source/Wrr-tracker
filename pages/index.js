8c75d1dfe3fd1d061b8baa18b602fcbad81d646f
Trey <wwrcanada@gmail.com>
2025-06-21 20:29:31 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
-
+import NavBar from "../components/NavBar";
 export default function Home() {
   const router = useRouter();
   const [user, setUser] = useState(null);
