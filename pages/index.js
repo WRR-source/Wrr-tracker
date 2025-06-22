034434c6d5c1965dd55c7cc4adab84b2f72e64c5
Trey <wwrcanada@gmail.com>
2025-06-21 20:48:55 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -41,6 +41,7 @@ export default function Home() {
 
   return (
     <div style={{ padding: 20, fontFamily: "sans-serif" }}>
+      <NavBar />
       <h1>Welcome, {user.name} (Unit {user.unit})</h1>
 
       <div style={{ marginTop: 20 }}>
