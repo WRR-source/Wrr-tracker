1cd35810022d41419a0a9e066aebe6eb452bbb53
Trey <wwrcanada@gmail.com>
2025-06-22 17:54:16 -0400

    Create admin.js

---
+++ pages/admin.js
@@ -0,0 +1,11 @@
+import NavBar from "../components/NavBar";
+
+export default function AdminPage() {
+  return (
+    <div>
+      <NavBar />
+      <h1>Admin Dashboard</h1>
+      <p>Welcome, admin! You’ll be able to manage users, add rewards, and more here.</p>
+    </div>
+  );
+}
